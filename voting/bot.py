d88755893c64c2a8ff99091d2b34e41ca726de35
Noah C. Green <noahc.green@icloud.com>
2021-03-27 15:24:18 -0400

    Basic conversation outline

--- voting/bot.py
+++ voting/bot.py
@@ -5,23 +5,34 @@ from discord.ext import commands
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='-', intents=intents)
 
+active_conversations = {}
+
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(bot))
+    print(f'We have logged in as {bot.user}')
 
 
-# @bot.event
-# async def on_message(message):
-#     if message.author == bot.user:
-#         return
-#     print(message.content)
-#
-#     if message.content.startswith('$hello'):
-#         await message.channel.send('Hello!')
-#         await message.author.send('Hi!')
-#
-#     await bot.process_commands(message)
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if (message.channel, message.author) in active_conversations:
+        conversation = active_conversations[(message.channel, message.author)]
+        try:
+            await conversation.asend(message)
+        except StopAsyncIteration:
+            del active_conversations[(message.channel, message.author)]
+
+    await bot.process_commands(message)
+
+
+async def create_vote_conversation(ctx, name):
+    await ctx.send(f'Tell me about the next choice for {name}, or react to this message to finish.')
+    message = yield
+    await ctx.send('kk')
+    await ctx.send(message.content)
 
 
 @bot.command('create-vote')
@@ -31,4 +42,6 @@ async def create_vote(ctx, *args):
         return
 
     name = ' '.join(args)
-    print(f'Creating vote named {name}')
+    conversation = create_vote_conversation(ctx, name)
+    active_conversations[(ctx.channel, ctx.author)] = conversation
+    await conversation.asend(None)
